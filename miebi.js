ea5746f5700028fd20d488258b0080935a456442
Miebifaghe Emmanuel <miebidrumzsep@gmail.com>
2026-02-16 16:38:44 +0100

    Add files via upload

---
+++ miebi.js
@@ -0,0 +1,5 @@
+const exploreBtn = document.querySelector(".btn");
+
+exploreBtn.addEventListener("click", (e) => {
+    alert("Welcome to the world of website design, and the beauty of it. Miebifaghe Emmanuel");
+})
