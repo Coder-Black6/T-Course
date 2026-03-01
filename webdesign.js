1bcf484ba119e8a06dfec9dd5f81c86c94e3db74
Miebifaghe Emmanuel <miebidrumzsep@gmail.com>
2026-03-01 12:09:26 +0100

    Add files via upload

---
+++ webdesign.js
@@ -0,0 +1,48 @@
+const modulesContainer = document.querySelector(".course-container");
+
+const modules =[
+    {
+        title : "Module 1",
+        image : "webdesign1.jpeg",
+        link :  "#"
+    },
+    {
+        title : "Module 2",
+        image : "webdesign1.jpeg",
+        link :  "#"
+    },
+    {
+        title : "Module 3",
+        image : "webdesign1.jpeg",
+        link :  "#"
+    },
+    {
+        title : "Module 4",
+        image : "webdesign1.jpeg",
+        link :  "#"
+    },
+    {
+        title : "Module 5",
+        image : "webdesign1.jpeg",
+        link :  "#"
+    },
+    {
+        title : "Module 6",
+        image : "webdesign1.jpeg",
+        link :  "#"
+    }
+]
+
+modules.forEach(module => {
+    
+    const card = document.createElement("div");
+    
+    card.className = "modules"
+    
+    card.innerHTML = `
+    <img src="${module.image}">
+    <p class="text">${module.title}</p>
+    <a href="${module.link}" class="btn">Click</a>
+    `;
+    modulesContainer.appendChild(card);
+})
