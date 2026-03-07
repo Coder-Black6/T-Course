e7bd2e4b89abd12543a307a061c20536df536834
Miebifaghe Emmanuel <miebidrumzsep@gmail.com>
2026-03-08 00:40:27 +0100

    Update miebi.js

--- miebi.js
+++ miebi.js
@@ -1,4 +1,4 @@
-const exploreBtn = document.querySelector("#btn");
+const exploreBtn = document.querySelector("#btn-submit");
 
 exploreBtn.addEventListener("click", (e) => {
     alert("Welcome to the world of website design, and the beauty of it. Miebifaghe Emmanuel");
