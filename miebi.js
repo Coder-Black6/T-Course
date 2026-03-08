fe7aa34a976be897c90532975dbfdf573b357d76
Miebifaghe Emmanuel <miebidrumzsep@gmail.com>
2026-03-08 01:01:53 +0100

    Update miebi.js

--- miebi.js
+++ miebi.js
@@ -5,5 +5,5 @@ const userEmail = document.getElementById("email");
 exploreBtn.addEventListener("click", (e) => {
     const nameCollector = fullName.textContent;
     
-    alert(`Welcome $(nameCollector.value) to the world of website design, and the beauty of it. Miebifaghe Emmanuel`);
+    alert(`Welcome $("nameCollector.value") to the world of website design, and the beauty of it. Miebifaghe Emmanuel`);
 })
